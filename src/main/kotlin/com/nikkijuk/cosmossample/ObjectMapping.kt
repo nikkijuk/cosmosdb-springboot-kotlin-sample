4d8465dc707f302c5f71cf9dd65aac9969e51e6b
Jukka Nikki <jukka.nikki@kolumbus.fi>
2021-04-09 11:33:05 +0200

    Updated dependencies and cleaned up mappings

--- src/main/kotlin/com/nikkijuk/cosmossample/ObjectMapping.kt
+++ src/main/kotlin/com/nikkijuk/cosmossample/ObjectMapping.kt
@@ -1,51 +1,29 @@
 package com.nikkijuk.cosmossample
 
-
 // to api
-
-// Type erasure removes here java typing and thus create method collision
-@JvmName("UserListToApi")
-fun List<com.nikkijuk.cosmossample.User>.toApi () =
-        this.map { it.toApi() }
-
 fun com.nikkijuk.cosmossample.User.toApi () =
         com.nikkijuk.model.User (
                 id = this.id,
                 firstName = this.firstName,
                 lastName = this.lastName,
-                addresses = this.addresses.toApi()
-        )
-
-// Type erasure removes here java typing and thus create method collision
-@JvmName("AddressListToApi")
-fun List<com.nikkijuk.cosmossample.Address>.toApi () =
-        this.map { it.toApi() }
+                addresses = this.addresses.map { it.toApi() })
 
 fun com.nikkijuk.cosmossample.Address.toApi () =
                 com.nikkijuk.model.Address (
                         street = this.street,
                         postalcode = this.postalcode,
-                        city = this.city
-                        )
-
+                        city = this.city)
 // to entity
-
 fun com.nikkijuk.model.User.toEntity () =
         com.nikkijuk.cosmossample.User (
                 id = this.id,
                 firstName = this.firstName,
                 lastName = this.lastName,
-                addresses = this.addresses.toEntity()
-        )
-
-// Type erasure removes here java typing and thus create method collision
-@JvmName("AddressListToEntityNullable")
-fun List<com.nikkijuk.model.Address>?.toEntity () =
-        this?.map { it.toEntity() } ?: listOf() // return empty list in case of null
+                // return empty list in case of null
+                addresses = this.addresses?.map { it.toEntity() } ?: listOf())
 
 fun com.nikkijuk.model.Address.toEntity () =
         com.nikkijuk.cosmossample.Address (
                 street = this.street,
                 postalcode = this.postalcode,
-                city = this.city
-        )
+                city = this.city)
